ec784dee8c9bf539746ed6ca3eba5ac3023054c4
Francesco <64654328+CaiaFra@users.noreply.github.com>
2021-02-20 15:33:40 +0100

    Delete public directory

--- public/js/main.js
+++
@@ -1,6 +0,0 @@
-window.onload = function (e) {
-   const flipCard = document.querySelectorAll('.flip-card');
-   [].forEach.call(flipCard, function (el) {
-      el.classList.add('active');
-   });
-};
